0d4e1aebb15de86b2b11f08901b91670158bbedd
Zetsumeii <theophile.combes@epitech.eu>
2024-10-14 10:18:26 +0200

    feat: marius can compile

---
+++ compile.sh
@@ -0,0 +1,5 @@
+conan profile detect || true
+conan install network --output-folder=network/build --build=missing
+cd network/build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build .
